e054c8366192113f1594dcc6776aa529d6950082
honorezemagho <honorezemagho@gmail.com>
2020-11-28 23:33:47 +0000

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+const config =  {
+    apiKey: "AIzaSyCroFlqCDzOaGVIxoJ56PyQfbzQSlORedc",
+    authDomain: "ecommerce-tracker-289ea.firebaseapp.com",
+    databaseURL: "https://ecommerce-tracker-289ea.firebaseio.com",
+    projectId: "ecommerce-tracker-289ea",
+    storageBucket: "ecommerce-tracker-289ea.appspot.com",
+    messagingSenderId: "125239719142",
+    appId: "1:125239719142:web:e0d2162338a0a0de83b727"
+  };
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   imports: [
     BrowserModule
